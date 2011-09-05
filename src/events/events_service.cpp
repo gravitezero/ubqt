7f05451dca988735a20d2f73bea16f0abe24709a
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2011-09-05 13:15:09 +0200

    events_service

--- src/events/events_service.cpp
+++ src/events/events_service.cpp
@@ -15,79 +15,29 @@
 namespace node {
 namespace events_service {
 
-server::server(const std::string& address, const std::string& port, const std::string& root_path)
-    : io_service_(),
-      //signals_(io_service_),
-      acceptor_(io_service_),
-      connection_manager_(),
-      new_connection_(),
-      request_handler_(root_path)
-{
-    // Register to handle the signals that indicate when the server should exit.
-    // It is safe to register for the same signal multiple times in a program,
-    // provided all registration for the specified signal is made through Asio.
-    /*signals_.add(SIGINT);
-    signals_.add(SIGTERM);
-    #if defined(SIGQUIT)
-        signals_.add(SIGQUIT);
-    #endif // defined(SIGQUIT)
-    signals_.async_wait(boost::bind(&server::handle_stop, this));
-*/
-    // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
-    boost::asio::ip::tcp::resolver resolver(io_service_);
-    boost::asio::ip::tcp::resolver::query query(address, port);
-    boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
-    acceptor_.open(endpoint.protocol());
-    acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-    acceptor_.bind(endpoint);
-    acceptor_.listen();
-
-  start_accept();
-}
-
-void server::run()
-{
-    // The io_service::run() call will block until all asynchronous operations
-    // have finished. While the server is running, there is always at least one
-    // asynchronous operation outstanding: the asynchronous accept call waiting
-    // for new incoming connections.
-    io_service_.run();
-}
-
-void server::start_accept()
-{
-    new_connection_.reset(new connection(io_service_,
-        connection_manager_, request_handler_));
-    acceptor_.async_accept(new_connection_->socket(),
-        boost::bind(&server::handle_accept, this,
-            boost::asio::placeholders::error));
-}
-
-void server::handle_accept(const boost::system::error_code& e)
-{
-    // Check whether the server was stopped by a signal before this completion
-    // handler had a chance to run.
-    if (!acceptor_.is_open())
+    void events_service::run()
     {
-        return;
+    
     }
 
-    if (!e)
+    // for a link, a set of events and callback functions associated should be set up.
+    // Let's do something really good with some boost::bind
+    int events_service::add_link(link_id id, map<event, functions>)
     {
-        connection_manager_.start(new_connection_);
+    
+    }
+    
+    // a link is actually a connection, so let's reuse this old class
+    int events_service::remove_link(link_id id)
+    {
+    
+    }
+    
+    // when an event is thrown, the events_service will call the associated function.
+    int events_service::throw_event(link_id id, event ev)
+    {
+    
     }
 
-    start_accept();
-}
-
-void server::handle_stop()
-{
-    // The server is stopped by cancelling all outstanding asynchronous
-    // operations. Once all operations have finished the io_service::run() call
-    // will exit.
-    acceptor_.close();
-    connection_manager_.stop_all();
-}
-
-} // namespace server
+} // namespace events_service
 } // namespace node
